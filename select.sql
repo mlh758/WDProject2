75cc7e8f14a098a566fe11bafa9ea37fa94d4ff6
Evan <etbcpb@mail.umkc.edu>
2015-11-02 19:22:19 -0600

    Select column from joined tables instead of actual table

--- select.sql
+++ select.sql
@@ -1,5 +1,5 @@
 --3.3 Returns the copy ID and book title of books that have been borrowed between 2010 and 2012
-SELECT DISTINCT loan.`copyID`, book.`Title`
+SELECT DISTINCT copyID, Title
 FROM copy
 INNER JOIN loan
 ON copy.`barCode` = loan.`copyID`
@@ -8,7 +8,7 @@ ON copy.`Book_ID` = book.`ISBN`
 WHERE year(borrowDate) <= 2012 && year(borrowDate) >= 2010
 
 --3.4 Returns the member ID, member name, copy ID, and book title of books that have not been returned
-SELECT loan.`memberID`, member.`Name`, loan.`copyID`, book.`Title`
+SELECT memberID, Name, copyID, Title
 FROM loan
 INNER JOIN member
 ON loan.`memberID` = member.`ID`
@@ -17,3 +17,10 @@ ON copy.`barCode` = loan.`copyID`
 INNER JOIN book
 ON copy.`Book_ID` = book.`ISBN`
 WHERE returnDate IS NULL && status = 1
+
+SELECT Title, COUNT(*)
+FROM copy
+INNER JOIN book
+ON copy.`Book_ID` = book.`ISBN`
+GROUP BY Book_ID
+ORDER BY COUNT(*) DESC, Title DESC
