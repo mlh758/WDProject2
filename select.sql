12c501cf34796f243d7fb0c3b87a4eba3db01df7
tKap <tamask@live.com>
2015-11-02 22:34:03 -0600

    Select Statements 3.1 and 3.2

--- select.sql
+++ select.sql
@@ -1,3 +1,19 @@
+--3.1 Return the names of authors who published books in 2002.
+select distinct author.Name
+from author
+inner join authorship
+on author.ID = authorship.Author_ID
+inner join book
+on authorship.Book_ID = book.ISBN
+where YearIssued = 2002;
+
+--3.2 Return the number of copies for each book.
+select Title, count(Book_ID) as "Number of Copies"
+from book
+inner join copy
+on book.ISBN = copy.Book_ID
+group by Book_ID;
+
 --3.3 Returns the copy ID and book title of books that have been borrowed between 2010 and 2012
 SELECT DISTINCT copyID, Title
 FROM copy
